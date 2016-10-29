d7018650301c41f4c0a6db34b2fb9e7b90b1b5b1
Dhanesh <d_budhrani90@hotmail.com>
2016-10-29 11:39:49 +0200

    Analysis class

---
+++ MicroCProject/src/models/Analysis.java
@@ -0,0 +1,29 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Analysis {
+
+    private ArrayList<Analysis> analyses;
+    private ArrayList<String> kills;
+    private ArrayList<String> gens;
+
+    public Analysis {
+        this.analyses = new ArrayList<Analysis>();
+        this.kills = new ArrayList<String>();
+        this.gens = new ArrayList<String>();
+    }
+
+    public void addAnalysis(Analysis _analysis) { this.analyses.add(_analysis); }
+
+    public ArrayList<Analysis> getAnalyses() { return analyses; }
+
+    public void addKill(String kill) { this.kills.add(kill); }
+
+    public ArrayList<String> getKills() { return kills; }
+
+    public void addGen(String gen) { this.gens.add(gen); }
+
+    public ArrayList<String> getGens() { return gens; }
+
+}
